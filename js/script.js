7132758523e66b85a14edcc1ea6be95ab967209d
Alessio Caruso <caruso.alessio1999@gmail.com>
2022-03-07 13:36:19 +0100

    optimized the code

--- js/script.js
+++ js/script.js
@@ -1,51 +1,34 @@
 let gridContainer=document.querySelector(".grid-easy");
-let gridContainerMidd=document.querySelector(".grid-middle");
-let gridContainerHard=document.querySelector(".grid-hard");
+// let gridContainerMidd=document.querySelector(".grid-middle");
+// let gridContainerHard=document.querySelector(".grid-hard");
 let choiseUser;
-console.log(gridContainer);
+
 document.querySelector('button').addEventListener('click', function(){
   choiseUser=document.querySelector("select").value;
   // reset when you click another level
   reset(gridContainer);
-  reset(gridContainerMidd);
-  reset(gridContainerHard);
 
   let squares;
+  let numberSquare=0;
+  gridContainer.innerHTML="";
   if(choiseUser === "easy"){
-    for (let i = 0; i < 100; i++) {
-      squares= createSquare();
-      squares.classList.add("easy");
-      gridContainer.classList.remove("d-none");
-      gridContainer.appendChild(squares);
-      // added click to square
-      squares.addEventListener('click', function() {
-        this.classList.toggle('active');
-      })
-    }
+    numberSquare=100;
   }
   else if(choiseUser === "middle"){
-    for (let i = 0; i < 81; i++) {
-      squares= createSquare();
-      squares.classList.add("middle");
-      gridContainerMidd.appendChild(squares);
-      gridContainerMidd.classList.remove("d-none");
-      // added click to square
-      squares.addEventListener('click', function(){
-        this.classList.toggle("active")
-      })
+      numberSquare=81;
     }
+  else {
+    numberSquare=49;
   }
-  else{
-    for (let i = 0; i < 49; i++) {
-      squares= createSquare();
-      squares.classList.add("hard");
-      gridContainerHard.appendChild(squares);
-      gridContainerHard.classList.remove("d-none")
-      // added click to square
-      squares.addEventListener('click', function(){
-        this.classList.toggle("active")
-      })
-    }
+  for (let i = 0; i < numberSquare; i++) {
+    squares= createSquare();
+    squares.classList.add(`${choiseUser}`); 
+    gridContainer.appendChild(squares);
+    gridContainer.classList.remove("d-none");
+    // added click to square
+    squares.addEventListener('click', function(){
+      this.classList.add("active");
+    })
   }
 })
 /** function that creates a div with class square
